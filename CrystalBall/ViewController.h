3599f80faf6494f5883795683e39e3d3809a7807
Kelsey Duffy <Kelsey@Kelseys-MacBook-Air.local>
2012-12-13 17:08:15 -0500

    Initial Commit

---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Kelsey Duffy on 12-12-13.
+//  Copyright (c) 2012 Kelsey Duffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
